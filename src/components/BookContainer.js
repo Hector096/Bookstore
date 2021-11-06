fab82a3cd7ef9a19f913f41a40711008527c29a4
Hector <vv96rca@gmail.com>
2021-11-06 18:00:34 +0530

    connected bootstore to the api

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
-import { Form, Button, Col } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import {
+  Form, Button, Col, Spinner,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
-import { addBook } from '../redux/books/books';
+import { addBook, fetchBooks } from '../redux/books/books';
 import './App.css';
 
 export default function BookContainer() {
   const [value, setValue] = useState({
     title: '',
-    author: '',
-    genre: '',
+    category: '',
   });
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+
   const addNewBook = (e) => {
     e.preventDefault();
     dispatch(addBook(value));
-    setValue({ title: '', author: '', genre: '' });
+    setValue({ title: '', category: '' });
   };
   const handleFormInput = (e) => {
     const inputName = e.target.name;
@@ -26,22 +31,23 @@ export default function BookContainer() {
 
   return (
     <>
-      <div className="book-container">
-        {books === undefined || books.length === 0 ? (
-          <h2 className="text-center fw-bold mt-5">No Books Found..</h2>
-        ) : (
-          books.map((item) => (
-            <Book data={item} key={item.id} />
-          ))
+      {books.loading !== true ? (
+        <div className="book-container">
+          {books === undefined || books.length === 0 ? (
+            <h2 className="text-center fw-bold mt-5">No Books Found..</h2>
+          ) : (
+            books.books.map((item) => (
+              <Book data={item} key={item.id} />
+            ))
 
-        )}
-      </div>
+          )}
+        </div>
+      ) : (<Spinner animation="grow" className="m-auto" size="lg" />)}
       <hr />
       <h2 className="ms-5 m-2 ps-2">Add Book</h2>
       <Form className="d-lg-flex ms-5" onSubmit={addNewBook}>
         <Col className="m-3"><Form.Control onChange={handleFormInput} value={value.title} type="text" placeholder="Book title" className="p-3 me-3" name="title" required /></Col>
-        <Col className="m-3"><Form.Control onChange={handleFormInput} value={value.author} type="text" placeholder="Author" className="p-3 me-3" name="author" required /></Col>
-        <Col className="m-3"><Form.Control onChange={handleFormInput} value={value.genre} type="text" placeholder="Category" className="p-3 me-3" name="genre" required /></Col>
+        <Col className="m-3"><Form.Control onChange={handleFormInput} value={value.category} type="text" placeholder="Category" className="p-3 me-3" name="category" required /></Col>
         <Col className="m-auto">
           <Button variant="primary" type="submit">
             Add book
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,53 +1,106 @@
+import axios from 'axios';
 import { v4 } from 'uuid';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOKS_REQUEST = 'bookStore/books/fetch_request';
+const FETCH_BOOKS_SUCCESS = 'bookStore/books/fetch_success';
+const FETCH_BOOKS_FAILURE = 'bookStore/books/fetch_failure';
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Ne1lNPOcr1S2yLU5FAOd/books';
 
-const initialState = [
-  {
-    title: 'Rich Dad Poor Dad',
-    author: 'Robert',
-    genre: 'Business',
-    completed: 56,
-    id: v4(),
-  },
-  {
-    title: 'Dune',
-    author: 'Frank Herbert',
-    genre: 'Science Fiction',
-    completed: 16,
-    id: v4(),
-  },
-  {
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    genre: 'Economy',
-    completed: 2,
-    id: v4(),
-  },
-];
+const initialState = {
+  loading: false,
+  books: [],
+  error: '',
+};
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
+export const fetchBooksRequest = () => ({
+  type: FETCH_BOOKS_REQUEST,
+});
+export const fetchBooksSuccess = (payload) => ({
+  type: FETCH_BOOKS_SUCCESS,
+  payload,
+});
+export const fetchBooksFailure = (payload) => ({
+  type: FETCH_BOOKS_FAILURE,
   payload,
 });
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
+export const fetchBooks = () => (dispatch) => {
+  dispatch(fetchBooksRequest());
+  axios
+    .get(BASE_URL, { headers: {} })
+    .then((response) => {
+      const data = Object.entries(response.data).map(([itemId, [book]]) => ({
+        id: itemId,
+        title: book.title,
+        category: book.category,
+      }));
+      dispatch(fetchBooksSuccess(data));
+    })
+    .catch((error) => {
+      fetchBooksFailure(error.message);
+    });
+};
+
+export const addBook = (payload) => (dispatch) => {
+  dispatch(fetchBooksRequest());
+  axios
+    .post(BASE_URL, {
+      item_id: v4(),
+      title: payload.title,
+      category: payload.category,
+    })
+    .then((response) => {
+      if (response.data === 'Created') {
+        dispatch(fetchBooks());
+      }
+    })
+    .catch((error) => {
+      fetchBooksFailure(error.message);
+    });
+};
+
+export const removeBook = (payload) => (dispatch) => {
+  dispatch(fetchBooksRequest());
+  axios
+    .delete(`${BASE_URL}/${payload.id}`)
+    .then((response) => {
+      if (response.data === 'The book was deleted successfully!') {
+        dispatch(fetchBooks());
+      }
+    })
+    .catch((error) => {
+      fetchBooksFailure(error.message);
+    });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_BOOKS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_BOOKS_SUCCESS:
+      return {
+        books: action.payload,
+        loading: false,
+        error: '',
+      };
+    case FETCH_BOOKS_FAILURE:
+      return {
+        error: action.payload,
+        loading: false,
+        books: [],
+      };
     case ADD_BOOK:
       return [
         ...state,
         {
           title: action.payload.title,
-          author: action.payload.author,
-          genre: action.payload.genre,
-          completed: 0,
-          id: v4(),
+          category: action.payload.genre,
+          id: action.payload.id,
         },
       ];
     case REMOVE_BOOK:
